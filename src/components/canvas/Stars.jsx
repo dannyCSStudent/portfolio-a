d1d328d0bb9bba86c3dec8f69daff95ceed6011d
dannyCSStudent <danv8605@gmail.com>
2023-08-05 13:56:22 -0600

    Fixed THREE.BufferGeometry Error

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -5,10 +5,13 @@ import * as random from 'maath/random/dist/maath-random.esm';
 
 const Stars = (props) => {
   const ref = useRef();
-  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2})
+  
+  const [sphere] = useState(() => random.inSphere(new Float32Array(5000 * 3), { radius: 1.2 }));
+  
+
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 16;
+    ref.current.rotation.y -= delta / 15;
 
   })
   return (
@@ -29,7 +32,7 @@ const Stars = (props) => {
 const StarsCanvas = () => {
   return (
     <div className='w-full h-auto absolute inset-0 z-[-1]'>
-      <Canvas camera={{ position: [0, 0, 1] }}>
+      <Canvas camera={{ position: [1, 1, 1] }}>
         <Suspense fallback={null}>
           <Stars />
         </Suspense>
